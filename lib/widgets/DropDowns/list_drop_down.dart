853ca9d20cf37ac7b0a9f065ec5bfa8f43a5927b
Nitish <vc.nitish.gupta@zee.com>
2023-07-25 19:39:44 +0530

    edittext field ui created

--- lib/widgets/DropDowns/list_drop_down.dart
+++ lib/widgets/DropDowns/list_drop_down.dart
@@ -1,24 +1,37 @@
 import 'package:bms_salesco/app/data/DropDownValue.dart';
+import 'package:bms_salesco/widgets/CustomSearchDropDown/src/popupMenu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:get/utils.dart';
 
+import '../../app/providers/SizeDefine.dart';
+
 class ListDropDown extends StatefulWidget {
-  final double widthRatio;
+  final List<DropDownValue> items;
   final String title;
+  final double widthRatio;
   final FocusNode? focusNode;
   final void Function(bool)? onFocusChange;
   final bool autoFocus;
   final DropDownValue? selected;
   final bool isEnable;
+  final GlobalKey? widgetKey;
+  final double? dialogWidth;
+  final double dialogHeight;
   const ListDropDown({
     super.key,
-    this.title = "",
+    required this.items,
+    required this.title,
     this.widthRatio = .15,
     this.focusNode,
     this.onFocusChange,
     this.autoFocus = false,
     this.selected,
     this.isEnable = true,
+    this.widgetKey,
+    this.dialogWidth,
+    this.dialogHeight = 200,
   });
 
   @override
@@ -28,77 +41,48 @@ class ListDropDown extends StatefulWidget {
 class _ListDropDownState extends State<ListDropDown> {
   late bool hasCurrentFocus;
   bool isDropDownOpen = false;
+  late GlobalKey widgetKey;
   @override
   void initState() {
     hasCurrentFocus = widget.autoFocus;
+    widgetKey = widget.widgetKey ?? GlobalKey();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          widget.title,
-          style: TextStyle(
-            fontWeight: hasCurrentFocus ? FontWeight.w600 : FontWeight.w400,
-            color: widget.isEnable ? Colors.black : Colors.grey,
-            fontSize: 12,
-          ),
-        ),
-        SizedBox(height: 3),
-        InkWell(
-          onFocusChange: (hasFocus) {
-            setState(() {
-              hasCurrentFocus = hasFocus;
-            });
-            if (widget.onFocusChange != null) {
-              widget.onFocusChange!(hasFocus);
-            }
-          },
-          focusNode: widget.focusNode,
-          autofocus: widget.autoFocus,
-          focusColor: Colors.white,
-          radius: 6,
-          onTap: () {
-            print(widget.title);
-          },
-          borderRadius: BorderRadius.circular(6),
-          child: Material(
-            child: Ink(
-              width: context.width * widget.widthRatio,
-              height: 30,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6),
-                border: Border.all(
-                  width: hasCurrentFocus ? 2.5 : 1,
-                  color: widget.isEnable ? Colors.deepPurple : Colors.grey,
-                ),
-                color: Colors.white,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    child: Text(
-                      widget.selected?.value ?? "",
-                      maxLines: 1,
-                      style: TextStyle(overflow: TextOverflow.ellipsis),
-                    ),
-                  ),
-                  Icon(
-                    isDropDownOpen ? Icons.arrow_drop_up_rounded : Icons.arrow_drop_down,
-                    color: widget.isEnable ? Colors.deepPurpleAccent : Colors.grey,
-                  ),
-                ],
-              ),
+    return SizedBox(
+      key: widgetKey,
+      width: context.width * widget.widthRatio,
+      height: 50,
+      child: InkWell(
+        onTap: () {
+          var isLoading = false.obs;
+          final RenderBox renderBox = widgetKey.currentContext?.findRenderObject() as RenderBox;
+          final offset = renderBox.localToGlobal(Offset.zero);
+          final left = offset.dx;
+          final top = offset.dy + renderBox.size.height;
+          final right = left + renderBox.size.width;
+          final width = renderBox.size.width;
+          var tempList = RxList<DropDownValue>([]);
+          tempList.addAll(widget.items);
+          showMenu(
+            context: context,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0),
             ),
-          ),
-        ),
-      ],
+            useRootNavigator: true,
+            position: RelativeRect.fromLTRB(left, top, right, 0.0),
+            constraints: BoxConstraints.expand(
+              width: widget.dialogWidth ?? width,
+              height: widget.dialogHeight,
+            ),
+            items: [
+              PopupMenuItem(child: TextFormField()),
+            ],
+          );
+        },
+      ),
     );
   }
 }
