02688651eb6f246e94add8135db6067e13102694
sanjayajena37 <sanjayajena00@gmail.com>
2023-07-27 14:53:28 +0530

    Added CommercialCreationAutoView.dart

--- lib/app/providers/ApiFactory.dart
+++ lib/app/providers/ApiFactory.dart
@@ -648,6 +648,10 @@ class ApiFactory {
   static String get MAKE_GOOD_REPORT_GET_BRAND => "$BASE_URL/api/MakeGoodReport/GetBrand";
   static String get MAKE_GOOD_REPORT_GET_GENERATE => "$BASE_URL/api/MakeGoodReport/Generate";
   ///////////////////////// MAKE-GOOD-REPORT-END//////
+  ///
+  ///
+  ///
+  ///
   ///////////////////////// COMERCIAL CREATION AUTO/////////
   static String COMMERCIAL_CREATION_GET_LOAD() => "$BASE_URL/api/CommercialMasterAuto/GetLoad";
   static String COMMERCIAL_CREATION_PROVIDER_LIST(String provider) => "$BASE_URL/api/CommercialMasterAuto/GetProviderSelect?Provider=$provider";
@@ -660,9 +664,6 @@ class ApiFactory {
   static String COMMERCIAL_CREATION_SELECT_CLIENT(String clientCode) => "$BASE_URL/api/CommercialMasterAuto/GetClientMasterByCode?SearchValue=$clientCode";
   static String COMMERCIAL_CREATION_SELECT_BRAND(String clientCode) => "$BASE_URL/api/CommercialMasterAuto/GetBrandMasterByCode?SearchValue=$clientCode";
   static String COMMERCIAL_CREATION_EOM_SELECT(String som,String eom) => "$BASE_URL/api/CommercialMasterAuto/GetEOMSelect?EOM=$som&SOM=$eom";
-  static String COMMERCIAL_CREATION_REVENUE_TYPE_SELECT(String revenue) =>
-      "$BASE_URL/api/CommercialMasterAuto/GetrevenuetypeSelect?revenuetype=$revenue";
-  static String COMMERCIAL_CREATION_EOM_SELECT(String som, String eom) => "$BASE_URL/api/CommercialMasterAuto/GetEOMSelect?EOM=$som&SOM=$eom";
   static String COMMERCIAL_CREATION_SAVE() => "$BASE_URL/api/CommercialMasterAuto/PostSave";
   ///////////////////////// End: COMERCIAL CREATION AUTO ////////////
 }
