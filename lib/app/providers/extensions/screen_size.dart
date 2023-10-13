c86324542c2dc92a7fac091d6c68f050cd5c0013
Nitish Gupta <vc.nitish.gupta@zee.com>
2023-10-13 15:21:47 +0530

    remove unwanted imports

--- lib/app/providers/extensions/screen_size.dart
+++ lib/app/providers/extensions/screen_size.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../widgets/CheckBox/multi_check_box.dart';
+
 extension DeviceSize on BuildContext {
   double get devicewidth => MediaQuery.of(this).size.width;
   double get deviceheight => MediaQuery.of(this).size.height;
