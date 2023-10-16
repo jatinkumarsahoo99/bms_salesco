9a0d5b7378ddaa67322b2f897d43a208663153e8
Nitish Gupta <vc.nitish.gupta@zee.com>
2023-10-16 19:39:56 +0530

    ro reschedule api integrated

--- lib/app/modules/ReleseOrderRescheduleTapeID/controllers/relese_order_reschedule_tape_i_d_controller.dart
+++ lib/app/modules/ReleseOrderRescheduleTapeID/controllers/relese_order_reschedule_tape_i_d_controller.dart
@@ -1,25 +1,379 @@
+import 'package:bms_salesco/app/controller/ConnectorControl.dart';
+import 'package:bms_salesco/app/data/DropDownValue.dart';
+import 'package:bms_salesco/app/providers/ApiFactory.dart';
+import 'package:bms_salesco/widgets/LoadingDialog.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pluto_grid/pluto_grid.dart';
+
+import '../model/ro_reschedule_tape_id_model.dart';
 
 class ReleseOrderRescheduleTapeIDController extends GetxController {
-  //TODO: Implement ReleseOrderRescheduleTapeIDController
+  RORescheduleOnLoadModel? onloadData;
+  List<LsttapeDetails>? lsttapeDetails;
+  PlutoGridStateManager? stateManager;
 
-  var dataTableList = [];
+  var lstBookingDetails = <LstBookingDetails>[].obs;
+  var locationList = <DropDownValue>[].obs,
+      channelList = <DropDownValue>[].obs,
+      clientList = <DropDownValue>[].obs,
+      agencyList = <DropDownValue>[].obs,
+      brandList = <DropDownValue>[].obs,
+      tapeList = <DropDownValue>[].obs,
+      tapeListRight = <DropDownValue>[].obs;
+  DropDownValue? selectedLocation,
+      selectedChannel,
+      selectedClient,
+      selectedAgency,
+      selectedBrand,
+      selectedTape,
+      selectedTapeRight;
+  var tapeCodeDura = "".obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
+  var fromDateTC = TextEditingController(), toDateTC = TextEditingController();
+
+  FocusNode locationFN = FocusNode();
 
   @override
   void onReady() {
     super.onReady();
+    getOnLoadData();
+  }
+
+  getOnLoadData() {
+    LoadingDialog.call();
+    Get.find<ConnectorControl>().GETMETHODCALL(
+      api: ApiFactory.RO_RESCHEDULE_TAPE_ID_ON_LOAD,
+      fun: (resp) {
+        Get.back();
+        if (resp != null) {
+          onloadData = RORescheduleOnLoadModel.fromJson(resp);
+          locationList.value.addAll(onloadData?.loadData?.lstLocation ?? []);
+        } else {
+          LoadingDialog.showErrorDialog(resp.toString());
+        }
+      },
+      failed: (resp) {
+        Get.back();
+        LoadingDialog.showErrorDialog(resp.toString());
+      },
+    );
+  }
+
+  getChannel(
+    DropDownValue? location,
+  ) {
+    if (location?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Location.");
+    } else {
+      LoadingDialog.call();
+      Get.find<ConnectorControl>().GETMETHODCALL(
+        api: ApiFactory.RO_RESCHEDULE_TAPE_ID_GET_CHANNELS(location!.key!),
+        fun: (resp) {
+          Get.back();
+          if (resp != null && resp["lstChannel"] != null) {
+            channelList.value = [];
+            channelList.value.addAll((resp['lstChannel'] as List<dynamic>)
+                .map((e) => DropDownValue(
+                      key: e['channelCode'],
+                      value: e['channelName'],
+                    ))
+                .toList());
+          } else {
+            LoadingDialog.showErrorDialog(resp.toString());
+          }
+        },
+        failed: (resp) {
+          Get.back();
+          LoadingDialog.showErrorDialog(resp.toString());
+        },
+      );
+    }
+  }
+
+  getClient(
+    DropDownValue? location,
+    DropDownValue? channel,
+  ) {
+    if (location?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Location.");
+    } else if (channel?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Channel.");
+    } else {
+      LoadingDialog.call();
+      Get.find<ConnectorControl>().GETMETHODCALL(
+        api: ApiFactory.RO_RESCHEDULE_TAPE_ID_GET_CLIENT(
+          location!.key!,
+          channel!.key!,
+        ),
+        fun: (resp) {
+          Get.back();
+          if (resp != null && resp["lstclient"] != null) {
+            clientList.value = [];
+            clientList.value.addAll((resp['lstclient'] as List<dynamic>)
+                .map((e) => DropDownValue(
+                      key: e['clientCode'],
+                      value: e['clientName'],
+                    ))
+                .toList());
+          } else {
+            LoadingDialog.showErrorDialog(resp.toString());
+          }
+        },
+        failed: (resp) {
+          Get.back();
+          LoadingDialog.showErrorDialog(resp.toString());
+        },
+      );
+    }
+  }
+
+  getAgency(
+    DropDownValue? location,
+    DropDownValue? channel,
+    DropDownValue? client,
+  ) {
+    if (location?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Location.");
+    } else if (channel?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Channel.");
+    } else if (client?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Client.");
+    } else {
+      LoadingDialog.call();
+      Get.find<ConnectorControl>().GETMETHODCALL(
+        api: ApiFactory.RO_RESCHEDULE_TAPE_ID_GET_AGENCY(
+          location!.key!,
+          channel!.key!,
+          client!.key!,
+        ),
+        fun: (resp) {
+          Get.back();
+          if (resp != null && resp["lstAgency"] != null) {
+            agencyList.value = [];
+            agencyList.value.addAll((resp['lstAgency'] as List<dynamic>)
+                .map((e) => DropDownValue(
+                      key: e['agencyCode'],
+                      value: e['agencyName'],
+                    ))
+                .toList());
+          } else {
+            LoadingDialog.showErrorDialog(resp.toString());
+          }
+        },
+        failed: (resp) {
+          Get.back();
+          LoadingDialog.showErrorDialog(resp.toString());
+        },
+      );
+    }
+  }
+
+  getBrand(
+    DropDownValue? location,
+    DropDownValue? channel,
+    DropDownValue? client,
+    DropDownValue? agency,
+  ) {
+    if (location?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Location.");
+    } else if (channel?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Channel.");
+    } else if (client?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Client.");
+    } else if (agency?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Agency.");
+    } else {
+      LoadingDialog.call();
+      Get.find<ConnectorControl>().GETMETHODCALL(
+        api: ApiFactory.RO_RESCHEDULE_TAPE_ID_GET_BRAND(
+          location!.key!,
+          channel!.key!,
+          client!.key!,
+          agency!.key!,
+        ),
+        fun: (resp) {
+          Get.back();
+          if (resp != null && resp["lstBrand"] != null) {
+            brandList.value = [];
+            brandList.value.addAll((resp['lstBrand'] as List<dynamic>)
+                .map((e) => DropDownValue(
+                      key: e['brandCode'],
+                      value: e['brandName'],
+                    ))
+                .toList());
+          } else {
+            LoadingDialog.showErrorDialog(resp.toString());
+          }
+        },
+        failed: (resp) {
+          Get.back();
+          LoadingDialog.showErrorDialog(resp.toString());
+        },
+      );
+    }
+  }
+
+  getTapeDetails(
+    DropDownValue? location,
+    DropDownValue? channel,
+    DropDownValue? client,
+    DropDownValue? agency,
+    DropDownValue? brand,
+    String fromDate,
+    String toDate,
+  ) {
+    if (location?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Location.");
+    } else if (channel?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Channel.");
+    } else if (client?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Client.");
+    } else if (agency?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Agency.");
+    } else if (brand?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Brand.");
+    } else {
+      LoadingDialog.call();
+      Get.find<ConnectorControl>().GETMETHODCALL(
+        api: ApiFactory.RO_RESCHEDULE_TAPE_ID_GET_TAPE_DETAILS(
+          location!.key!,
+          channel!.key!,
+          client!.key!,
+          agency!.key!,
+          brand!.key!,
+          fromDate,
+          toDate,
+        ),
+        fun: (resp) {
+          Get.back();
+          if (resp != null && resp["lsttapeDetails"] != null) {
+            lsttapeDetails = [];
+            for (var element in resp['lsttapeDetails']) {
+              lsttapeDetails!.add(LsttapeDetails.fromJson(element));
+            }
+            tapeList.value = [];
+            tapeList.value.addAll(lsttapeDetails
+                    ?.map((e) => DropDownValue(
+                          key: (e.duration ?? "").toString(),
+                          value: e.exportTapeCode,
+                        ))
+                    .toList() ??
+                []);
+          } else {
+            LoadingDialog.showErrorDialog(resp.toString());
+          }
+        },
+        failed: (resp) {
+          Get.back();
+          LoadingDialog.showErrorDialog(resp.toString());
+        },
+      );
+    }
+  }
+
+  getBookingDetails(
+    DropDownValue? location,
+    DropDownValue? channel,
+    DropDownValue? client,
+    DropDownValue? agency,
+    DropDownValue? brand,
+    String fromDate,
+    String toDate,
+    DropDownValue? tapeCode,
+  ) {
+    if (location?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Location.");
+    } else if (channel?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Channel.");
+    } else if (client?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Client.");
+    } else if (agency?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Agency.");
+    } else if (brand?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Brand.");
+    } else if (tapeCode?.key == null) {
+      LoadingDialog.callInfoMessage("Please select Tape Code.");
+    } else {
+      LoadingDialog.call();
+      Get.find<ConnectorControl>().GETMETHODCALL(
+        api: ApiFactory.RO_RESCHEDULE_TAPE_ID_GET_TAPE_BOOKING_DETAILS(
+          location!.key!,
+          channel!.key!,
+          client!.key!,
+          agency!.key!,
+          brand!.key!,
+          fromDate,
+          toDate,
+          tapeCode!.value!,
+        ),
+        fun: (resp) {
+          Get.back();
+          if (resp != null && resp["lstBookingDetails"] != null) {
+            lstBookingDetails.value = [];
+            lstBookingDetails.addAll(
+                (resp['lstBookingDetails'] as List<dynamic>)
+                    .map((e) => LstBookingDetails.fromJson(e))
+                    .toList());
+            tapeListRight.value = [];
+            if (tapeCodeDura.value == "") {
+              tapeCodeDura.value = "0";
+            }
+
+            for (var i = 0; i < tapeList.length; i++) {
+              if (tapeList[i].value != selectedTape!.value &&
+                  tapeList[i].key == tapeCodeDura.value) {
+                tapeListRight.add(tapeList[i]);
+              }
+              // if (lstBookingDetails.any((element) =>
+              //     element.exportTapeCode != selectedTape!.value &&
+              //     (element.tapeDuration).toString() == tapeCodeDura.value)) {
+              //   tapeListRight.add(tapeList[i]);
+              // }
+            }
+            if (tapeListRight.isEmpty) {
+              LoadingDialog.callInfoMessage(
+                  "No optional TapeID found for replacing existing TapeID.");
+            }
+          } else {
+            LoadingDialog.showErrorDialog(resp.toString());
+          }
+        },
+        failed: (resp) {
+          Get.back();
+          LoadingDialog.showErrorDialog(resp.toString());
+        },
+      );
+    }
+  }
+
+  saveData(
+    String? modifiedBy,
+    DropDownValue? location,
+    DropDownValue? channel,
+    DropDownValue? client,
+    DropDownValue? agency,
+    DropDownValue? brand,
+    String? formDate,
+    String? toDate,
+    LsttapeDetails? exportTapeCode,
+  ) {}
+
+  changeExportTapeCode() {
+    if (selectedTapeRight == null) {
+      LoadingDialog.callInfoMessage("Please replaceble Tapecode.");
+    } else {
+      for (var i = 0; i < lstBookingDetails.length; i++) {
+        if (lstBookingDetails[i].action ?? false) {
+          lstBookingDetails[i].exportTapeCode = selectedTapeRight!.value!;
+        }
+      }
+      lstBookingDetails.refresh();
+    }
   }
 
   @override
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/ReleseOrderRescheduleTapeID/views/relese_order_reschedule_tape_i_d_view.dart
+++ lib/app/modules/ReleseOrderRescheduleTapeID/views/relese_order_reschedule_tape_i_d_view.dart
@@ -7,6 +7,7 @@ import 'package:bms_salesco/widgets/gridFromMap.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 import '../../../../widgets/dropdown.dart';
 import '../../../../widgets/input_fields.dart';
@@ -33,35 +34,133 @@ class ReleseOrderRescheduleTapeIDView extends StatelessWidget {
                     spacing: 10,
                     runSpacing: 5,
                     crossAxisAlignment: WrapCrossAlignment.end,
-                    // runAlignment: WrapAlignment.end,
-                    // alignment: WrapAlignment.end,
                     children: [
-                      DropDownField.formDropDown1WidthMap(
-                          [], (p0) => null, "Location", .12),
-                      DropDownField.formDropDown1WidthMap(
-                          [], (p0) => null, "Channel", .12),
-                      DropDownField.formDropDown1WidthMap(
-                          [], (p0) => null, "Client", .12),
-                      DropDownField.formDropDown1WidthMap(
-                          [], (p0) => null, "Brand", .12),
-                      DropDownField.formDropDown1WidthMap(
-                          [], (p0) => null, "Tape Code", .12),
-                      InputFields.formField1(
-                        hintTxt: "TapeCode Duration",
-                        controller: TextEditingController(),
-                      ),
+                      Obx(() {
+                        return DropDownField.formDropDown1WidthMap(
+                          controller.locationList.value,
+                          (val) {
+                            controller.selectedLocation = val;
+                            controller.getChannel(val);
+                          },
+                          "Location",
+                          .12,
+                          selected: controller.selectedLocation,
+                          inkWellFocusNode: controller.locationFN,
+                          autoFocus: true,
+                        );
+                      }),
+                      Obx(() {
+                        return DropDownField.formDropDown1WidthMap(
+                          controller.channelList.value,
+                          (val) {
+                            controller.selectedChannel = val;
+                            controller.getClient(
+                              controller.selectedLocation,
+                              val,
+                            );
+                          },
+                          "Channel",
+                          .12,
+                          selected: controller.selectedChannel,
+                        );
+                      }),
+                      Obx(() {
+                        return DropDownField.formDropDown1WidthMap(
+                          controller.clientList.value,
+                          (val) {
+                            controller.selectedClient = val;
+                            controller.getAgency(
+                              controller.selectedLocation,
+                              controller.selectedChannel,
+                              val,
+                            );
+                          },
+                          "Client",
+                          .12,
+                          selected: controller.selectedClient,
+                          dialogWidth: 300,
+                        );
+                      }),
+                      Obx(() {
+                        return DropDownField.formDropDown1WidthMap(
+                          controller.agencyList.value,
+                          (val) {
+                            controller.selectedAgency = val;
+                            controller.getBrand(
+                              controller.selectedLocation,
+                              controller.selectedChannel,
+                              controller.selectedClient,
+                              val,
+                            );
+                          },
+                          "Agency",
+                          .12,
+                          selected: controller.selectedAgency,
+                          dialogWidth: 300,
+                        );
+                      }),
+                      Obx(() {
+                        return DropDownField.formDropDown1WidthMap(
+                          controller.brandList.value,
+                          (val) {
+                            controller.selectedBrand = val;
+                            controller.getTapeDetails(
+                              controller.selectedLocation,
+                              controller.selectedChannel,
+                              controller.selectedClient,
+                              controller.selectedAgency,
+                              val,
+                              controller.fromDateTC.text,
+                              controller.toDateTC.text,
+                            );
+                          },
+                          "Brand",
+                          .12,
+                          selected: controller.selectedBrand,
+                        );
+                      }),
+                      Obx(() {
+                        return DropDownField.formDropDown1WidthMap(
+                          controller.tapeList.value,
+                          (val) {
+                            controller.selectedTape = val;
+                            controller.tapeCodeDura.value = val.key!;
+                          },
+                          "Tape Code",
+                          .12,
+                          selected: controller.selectedTape,
+                        );
+                      }),
+                      Obx(() {
+                        return InputFields.formFieldDisable1(
+                          hintTxt: "TapeCode Duration",
+                          value: controller.tapeCodeDura.value,
+                          widthRatio: .12,
+                        );
+                      }),
                       Row(),
                       DateWithThreeTextField(
-                          title: "Eff. From Date",
-                          widthRation: .12,
-                          mainTextController: TextEditingController()),
+                        title: "Eff. From Date",
+                        widthRation: .12,
+                        mainTextController: controller.fromDateTC,
+                      ),
                       DateWithThreeTextField(
-                          widthRation: .12,
-                          title: "Eff. To Date",
-                          mainTextController: TextEditingController()),
+                        widthRation: .12,
+                        title: "Eff. To Date",
+                        mainTextController: controller.toDateTC,
+                      ),
                       FormButton(
                         btnText: "Search",
-                        callback: () {},
+                        callback: () => controller.getBookingDetails(
+                          controller.selectedLocation,
+                          controller.selectedChannel,
+                          controller.selectedClient,
+                          controller.selectedAgency,
+                          controller.selectedBrand,
+                          controller.fromDateTC.text,
+                          controller.toDateTC.text,
+                          controller.selectedTape,
+                        ),
                       ),
                       FormButton(
                         btnText: "Clear",
@@ -73,8 +172,83 @@ class ReleseOrderRescheduleTapeIDView extends StatelessWidget {
 
                   /// Data table
                   Expanded(
-                    child: DataGridFromMap3(
-                      mapData: controller.dataTableList,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Obx(() {
+                            return DataGridFromMap3(
+                              columnAutoResize: false,
+                              mapData: controller.lstBookingDetails.value
+                                  .map((e) => e.toJson())
+                                  .toList(),
+                              formatDate: false,
+                              hideCode: false,
+                              colorCallback: (event) {
+                                if (event.row.cells['edit']?.value == "1") {
+                                  return const Color.fromARGB(
+                                      255, 128, 128, 128);
+                                }
+                                if (event.row.cells['bookingStatus']?.value ==
+                                    "R") {
+                                  return const Color.fromARGB(
+                                      255, 188, 143, 143);
+                                }
+                                return Colors.white;
+                              },
+                              checkBoxStrComparison: "true",
+                              uncheckCheckBoxStr: "false",
+                              checkBoxColumnKey: ["action"],
+                              actionIconKey: ["action"],
+                              onload: (loadEvent) {
+                                controller.stateManager =
+                                    loadEvent.stateManager;
+                              },
+                              actionOnPress: (position, isSpaceCalled) {
+                                if (isSpaceCalled) {
+                                  var newVal = !(controller
+                                          .stateManager!
+                                          .refRows[position.rowIdx!]
+                                          .cells['action']!
+                                          .value ==
+                                      "true");
+                                  controller
+                                      .lstBookingDetails[controller
+                                          .stateManager!
+                                          .refRows[position.rowIdx!]
+                                          .sortIdx]
+                                      .action = newVal;
+                                }
+                              },
+                              onEdit: (event) {
+                                controller.lstBookingDetails[event.row.sortIdx]
+                                    .action = (event.value == "true");
+                              },
+                            );
+                          }),
+                        ),
+                        SizedBox(
+                          width: context.devicewidth * .3,
+                          child: Column(
+                            children: [
+                              Obx(() {
+                                return DropDownField.formDropDown1WidthMap(
+                                  controller.tapeListRight.value,
+                                  (val) {
+                                    controller.selectedTapeRight = val;
+                                  },
+                                  "Tape Code",
+                                  .12,
+                                  selected: controller.selectedTapeRight,
+                                );
+                              }),
+                              FormButton(
+                                btnText: "Modify",
+                                callback: controller.changeExportTapeCode,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   const SizedBox(height: 5),
