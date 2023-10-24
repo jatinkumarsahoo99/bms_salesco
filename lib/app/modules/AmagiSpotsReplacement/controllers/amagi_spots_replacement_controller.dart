77be08a989fcf6359f64ba872ab8570dda57e0f1
vc.jatinku.sahoo <vc.jatinku.sahoo@zee.com>
2023-10-24 17:31:05 +0530

    One spot booking bugs resolved

--- lib/app/modules/AmagiSpotsReplacement/controllers/amagi_spots_replacement_controller.dart
+++ lib/app/modules/AmagiSpotsReplacement/controllers/amagi_spots_replacement_controller.dart
@@ -342,7 +342,7 @@ class AmagiSpotsReplacementController extends GetxController {
                   Map<String, dynamic> mapDa = {};
                   element.forEach((key, value) {
                     String k = key.toString().trim().replaceAll("\n", " ");
-                    mapDa[k] = value;
+                    mapDa[k.toLowerCase()] = value;
                   });
                   mapData.add(mapDa);
                 }
