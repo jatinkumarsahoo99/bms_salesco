13ee23f8f1cc59b51da4d95cc497e391dfad972b
jatinkumarsahoo <jatnkumarsahoo99@gmail.com>
2023-10-30 17:30:41 +0530

    Update executive bugs resolved

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -58,7 +58,7 @@ class AppPages {
 
   static const INITIAL = kReleaseMode
       ? Routes.HOME
-      : (Routes.AUTO_TIME_LOCK +
+      : (Routes.UPDATE_EXECUTIVE +
           "?personalNo=R9vVPL7er1Os/usemWG/Iw==&loginCode=0iGe3vK5h2KGjfSKZTpmsQ==&formName=OI8ukDpPPVN0I2BEXu2h4nuFu%2BZm1ZRpvP8NL4XCXzQ%3D");
 
   static final routes = [
