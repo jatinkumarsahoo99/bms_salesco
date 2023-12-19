6938c84146c3c913de14a4c22e53deb1fc573ea4
Mohd Maroof <vc.mohd.maroof@zee.com>
2023-12-19 18:22:44 +0530

    bugs fixed.

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -44,7 +44,7 @@ class AppPages {
 
   static const INITIAL = kReleaseMode
       ? Routes.HOME
-      : (Routes.MAKE_GOOD_REPORT +
+      : (Routes.EDI_RO_BOOKING +
           "?personalNo=R9vVPL7er1Os/usemWG/Iw==&loginCode=0iGe3vK5h2KGjfSKZTpmsQ==&formName=OI8ukDpPPVN0I2BEXu2h4nuFu%2BZm1ZRpvP8NL4XCXzQ%3D");
 
   static final routes = [
