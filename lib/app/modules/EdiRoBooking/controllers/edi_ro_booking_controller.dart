fb6522695304d53aefe1153880708172051f53f2
Mohd Maroof <vc.mohd.maroof@zee.com>
2024-01-18 17:22:59 +0530

    edi bugs fixed.

--- lib/app/modules/EdiRoBooking/controllers/edi_ro_booking_controller.dart
+++ lib/app/modules/EdiRoBooking/controllers/edi_ro_booking_controller.dart
@@ -1917,14 +1917,16 @@ class EdiRoBookingController extends GetxController {
         }).toList(),
         "refNo": selectedRoRefNo?.value ?? "",
       };
-      Get.find<ConnectorControl>().POSTMETHOD(
-          api: ApiFactory.EDI_RO_GET_EXCEL_FILE_DOWNLOAD,
-          json: payload,
+      Get.find<ConnectorControl>().GETMETHODCALL(
+          api:
+              'https://api-programming-bms-dev.zeeconnect.in/api/MoviePlanner/GetExcelFPCDownload?Locationcode=ZAZEE00001&channelcode=ZAFLI00001&Date=2024-01-18&OptBMSProgram=true&OptViewProg=false',
+          // ApiFactory.EDI_RO_GET_EXCEL_FILE_DOWNLOAD,
+          // json: payload,
           fun: (map) {
             Get.back();
             if (map != null) {
-              ExportData().exportFilefromByte(
-                  map, "${selectedRoRefNo?.value ?? ""}.xlsx");
+              ExportData()
+                  .exportFilefromByte(map, "${selectedRoRefNo?.value ?? ""}");
             }
           });
     } catch (e) {
